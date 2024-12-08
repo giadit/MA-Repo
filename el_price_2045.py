e6042588ad8ed4b5bddef109049661ded1f5df53
Giampaolo Di Tomassi <161382440+giadit@users.noreply.github.com>
2024-12-08 11:56:50 +0100

    corrected el_price_2045.py and added diagrams_2045.py

--- el_price_2045.py
+++ el_price_2045.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
-df = pd.read_csv("data/energy-charts_Stromproduktion_und_Börsenstrompreise_in_Deutschland_2019.csv", delimiter=";", index_col=0)
+df = pd.read_csv("data/energy-charts_Electricity_production_and_spot_prices_in_Germany_in_2022.csv", delimiter=";", index_col=0)
 
 corr = {"1": 92.0315557,
         "2": 122.6917515,
@@ -15,18 +15,18 @@ corr = {"1": 92.0315557,
         "11":115.3223611,
         "12":109.5987942}
 segments = {
-    "1": df["Preis (EUR/MWh, EUR/tCO2)"].iloc[0:744],
-    "2": df["Preis (EUR/MWh, EUR/tCO2)"].iloc[744:1416],
-    "3": df["Preis (EUR/MWh, EUR/tCO2)"].iloc[1416:2160],
-    "4": df["Preis (EUR/MWh, EUR/tCO2)"].iloc[2160:2880],
-    "5": df["Preis (EUR/MWh, EUR/tCO2)"].iloc[2880:3624],
-    "6": df["Preis (EUR/MWh, EUR/tCO2)"].iloc[3624:4344],
-    "7": df["Preis (EUR/MWh, EUR/tCO2)"].iloc[4344:5088],
-    "8": df["Preis (EUR/MWh, EUR/tCO2)"].iloc[5088:5832],
-    "9": df["Preis (EUR/MWh, EUR/tCO2)"].iloc[5832:6552],
-    "10": df["Preis (EUR/MWh, EUR/tCO2)"].iloc[6552:7296],
-    "11": df["Preis (EUR/MWh, EUR/tCO2)"].iloc[7296:8016],
-    "12": df["Preis (EUR/MWh, EUR/tCO2)"].iloc[8016:8760]}
+    "1": df["Price (EUR/MWh, EUR/tCO2)"].iloc[0:744],
+    "2": df["Price (EUR/MWh, EUR/tCO2)"].iloc[744:1416],
+    "3": df["Price (EUR/MWh, EUR/tCO2)"].iloc[1416:2160],
+    "4": df["Price (EUR/MWh, EUR/tCO2)"].iloc[2160:2880],
+    "5": df["Price (EUR/MWh, EUR/tCO2)"].iloc[2880:3624],
+    "6": df["Price (EUR/MWh, EUR/tCO2)"].iloc[3624:4344],
+    "7": df["Price (EUR/MWh, EUR/tCO2)"].iloc[4344:5088],
+    "8": df["Price (EUR/MWh, EUR/tCO2)"].iloc[5088:5832],
+    "9": df["Price (EUR/MWh, EUR/tCO2)"].iloc[5832:6552],
+    "10": df["Price (EUR/MWh, EUR/tCO2)"].iloc[6552:7296],
+    "11": df["Price (EUR/MWh, EUR/tCO2)"].iloc[7296:8016],
+    "12": df["Price (EUR/MWh, EUR/tCO2)"].iloc[8016:8760]}
 
 for i in range(12):
         segments[f"{i+1}"]=(corr[f"{i+1}"]/segments[f"{i+1}"].mean())*segments[f"{i+1}"]
